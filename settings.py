737da2369d923633e8b57885aacd503044fe5889
Daniel Santos <dfsantosbu@unal.edu.co>
2018-01-17 10:21:06 -0500

    Change some variables fon debugging purposes.

--- settings.py
+++ settings.py
@@ -107,10 +107,12 @@ SESSION_CONFIGS = [
         {
             'name': 'base_game',
             'display_name': "Base Game test case",
-            'num_demo_participants': 8,
+            #'num_demo_participants': 8,
+            'num_demo_participants': 2,
             'endowment': 20,
             'community_goal_decimal': .01,
-            'app_sequence': ['survey', 'public_goods'],
+            #'app_sequence': ['survey', 'public_goods'],
+            'app_sequence': ['public_goods'],
             'doc': """community_goal_decimal limits: [0,1]"""
         },
 ]
